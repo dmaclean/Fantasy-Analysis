d2eec2c87519a7c102f5d3cf1b410127de18abff
Dan MacLean <dmaclean82@gmail.com>
2013-01-03 09:47:15 -0500

    Added a convenience method that resets the test database by running the create_tables script.  This can be used after each unit test to clear out the database.

--- test/com/dmaclean/testutils/FantasyTestUtils.java
+++ test/com/dmaclean/testutils/FantasyTestUtils.java
@@ -1,11 +1,22 @@
 package com.dmaclean.testutils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class FantasyTestUtils {
+	
+	private static final Logger logger = Logger.getLogger(FantasyTestUtils.class.getPackage().getName());
+	
 	/**
-	 * 
+	 * Create a JDBC connection to the test database.
 	 * 
 	 * @return
 	 */
@@ -25,5 +36,60 @@ public class FantasyTestUtils {
 		}
 		
 		return conn;
+	}
+	
+	/**
+	 * Run the create_tables.sql script to reset the test database tables.
+	 */
+	public static void resetTestDatabase() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		long start = System.currentTimeMillis();
+		
+		try {
+			BufferedReader r = new BufferedReader(new FileReader(new File("sql/create_tables.sql")));
+			ArrayList<String> commands = new ArrayList<String>();
+			StringBuffer sb = new StringBuffer();
+			String s = null;
+			
+			while( (s = r.readLine()) != null) {
+				sb.append(s);
+				
+				// We've reached the end of a command.
+				if(s.contains(";")) {
+					commands.add(sb.toString());
+					sb = new StringBuffer();
+				}
+			}
+			
+			conn = FantasyTestUtils.getTestConnection();
+			
+			for(String command : commands) {
+				pstmt = conn.prepareStatement(command);
+				pstmt.execute();
+			}
+		}
+		catch(Exception e) {
+			logger.severe(e.getMessage());
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+				pstmt.close();
+			}
+			catch(SQLException e) {
+				logger.severe(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		
+		long end = System.currentTimeMillis();
+		logger.info("Reset test database in " + (end-start)/1000.0 + " seconds.");
+	}
+	
+	public static void main(String[] args) {
+		FantasyTestUtils.resetTestDatabase();
 	}
 }
