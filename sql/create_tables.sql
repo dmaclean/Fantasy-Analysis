e97dfd4985c6d75899703651e788a95548d151d3
Dan MacLean <dmaclean82@gmail.com>
2013-02-05 20:46:32 -0500

    Added tables for fantasy_points and team_membership.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -16,3 +16,23 @@ create table stats (
 	foreign key (player_id) references player(id)
 );
 create index stat_key_idx on stats(stat_key);
+
+drop table fantasy_points;
+create table fantasy_points (
+	id int auto_increment primary key,
+	player_id int not null,
+	system varchar(100) not null,
+	season int not null,
+	week int not null,
+	points int not null
+);
+
+drop table team_membership;
+create table team_membership (
+	id int auto_increment primary key,
+	player_id int not null,
+	season int not null,
+	team varchar(5) not null,
+	version int,
+	foreign key (player_id) references player(id)
+);
